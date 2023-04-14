90a7befea3fc7166cda6dfd7d979595516332762
Adinda Zahra <zahraadinda171@gmail.com>
2023-04-14 15:37:58 +0700

    first commit

---
+++ project_2/lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+  int? result = 0, num1 = 0, num2 = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Calculator"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text(
+                "Result : $result",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: controller1,
+                decoration: InputDecoration(
+                    labelText: "Enter First Number",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: controller2,
+                decoration: InputDecoration(
+                    labelText: "Enter Second Number",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Color(0xffF18265)),
+                      onPressed: () {},
+                      child: Text("ADD")),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Color(0xffF18265)),
+                      onPressed: () {},
+                      child: Text("SUBSTRACT")),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Color(0xffF18265)),
+                      onPressed: () {},
+                      child: Text("MULTIPLY")),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Color(0xffF18265)),
+                      onPressed: () {},
+                      child: Text("DIVIDE")),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
